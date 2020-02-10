b9d40eec0215ea0f2ac8ab4a9b92feaac999042c
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-02-10 23:45:33 +0900

    Create expressSample.js

---
+++ expressSample.js
@@ -0,0 +1,22 @@
+// Flutter X 생활코딩(Node.js/express) # 2
+
+var express = require("express");
+var app = express();
+
+app.get("/", (req,res)=>{
+    // res.send("기존 예제");
+    res.json(
+        [
+            {
+                'id' : 1,
+                'name' : 'K'
+            },
+            {
+                'id' : 2,
+                'name' : 'S'
+            },
+        ]
+    )
+});
+
+app.listen( 3000, (err) => console.log(3000) );
