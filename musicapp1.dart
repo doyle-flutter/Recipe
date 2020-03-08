200d9a8106922d12d9694509c6d2c5dbf7964600
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-03-08 20:06:30 +0900

    Update musicapp1.dart

--- musicapp1.dart
+++ musicapp1.dart
@@ -47,7 +47,6 @@ class _MyAppState extends State<MyApp> {
       appBar: AppBar(
         title: Text("MainPage"),
         centerTitle: true,
-        leading: Container(),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.confirmation_number),
