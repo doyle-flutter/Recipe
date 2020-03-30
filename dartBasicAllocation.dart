4b3ec2c58dca8d7248a4d3a3ea16bb1d6b3ca9bf
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-03-31 01:38:56 +0900

    ..ing
    ..ing

--- dartBasicAllocation.dart
+++ dartBasicAllocation.dart
@@ -1,43 +1,43 @@
-// ... ing
-
 void main() {
 
   // value context
+  var vVar = "동적으로(아무거나)) 담을 수 있음"; 
+  dynamic vdynamic = '아무거나';
+  String vString = "반드시 문자열만";
+  int vInt = 1;
+  double vDouble = 2.0;
+  bool vBool = true;
+  void vVoiid = null;
+  List vlist = new List();
+  Set vSet = new Set();
+  Function vFunction = (){};
+  MyClass vMyClass = new MyClass();
   
-  var a = "동적으로(아무거나)) 담을 수 있음"; 
-  dynamic b = '아무거나';
-  String c = "반드시 문자열만";
-  int d = 1;
-  double e = 2.0;
-  bool f = true;
-  void g = null;
-  Function h = (){};
-  MyClass i = new MyClass();
   
   // identify context
-  List lvar = [];
+  List lVar = [];
   List<dynamic> ldynamic = [1,"2",false];
-  List<String> lstring = ["리스트", "안에", "반드시", "문자만"];
-  List<int> lint = [1,2,3];
-  List<double> ldouble = [1.0, 2.0, 3.0];
-  List<bool> lbool = [false, true, false];
-  List<void> lvoid = [null, null, null];
-  List<List> llist = [[],[],[]];
-  List<Set> lset = [new Set(), new Set.from([]), new Set()];
-  List<Map> lmap = [{}, {}];
-  List<Function> lfunction = [(){}, (){}, (){}];
-  List<MyClass> lmyClass = [new MyClass(), new MyClass()];
+  List<String> lString = ["리스트", "안에", "반드시", "문자만"];
+  List<int> lInt = [1,2,3];
+  List<double> lDouble = [1.0, 2.0, 3.0];
+  List<bool> lBool = [false, true, false];
+  List<void> lVoid = [null, null, null];
+  List<List> lList = [[],[],[]];
+  List<Set> lSet = [new Set(), new Set.from([]), new Set()];
+  List<Map> lMap = [{}, {}];
+  List<Function> lFunction = [(){}, (){}, (){}];
+  List<MyClass> lMyClass = [new MyClass(), new MyClass()];
   
-  List<List<String>> llstring = [ ["a"], ["b"], ["c"] ];
-  List<List<int>> llint = [[1], [2], [3]];
-  List<List<double>> lldouble = [[1.0],[2.0],[3]];
-  List<List<bool>> llbool = [[false], [true], [false]];
-  List<List<void>> llvoid = [[null],[null],[null]];
-  List<List<Map>> llmap = [[{}],[{}],[{}]];
-  List<List<MyClass>> llMyclass = [[new MyClass()],[new MyClass()],[new MyClass()]];
-  List<List<Function>> llfunction = [[(){}],[(){}],[(){}]];
-  
-  
+  List<List<String>> llString = [ ["a"], ["b"], ["c"] ];
+  List<List<int>> llInt = [[1], [2], [3]];
+  List<List<double>> llDouble = [[1.0],[2.0],[3]];
+  List<List<bool>> llBool = [[false], [true], [false]];
+  List<List<void>> llVoid = [[null],[null],[null]];
+  List<List<List>> llList = [[[]],[[]],[[]], new List.from([new List()])];
+  List<List<Set>> llSet = [[new Set()],[new Set()],[new Set.from([])]];
+  List<List<Map>> llMap = [[{}],[{}],[{}]];
+  List<List<Function>> llFunction = [[(){}],[(){}],[(){}]];
+  List<List<MyClass>> llMyclass = [[new MyClass()],[new MyClass()]];
   
   Set sa = new Set();
   Set sb = new Set.from([1,'2','2',false, false, new MyClass(), new MyClass()]);
@@ -46,12 +46,32 @@ void main() {
   Set<double> se = new Set.from([1.0, 1.0, 3.0]);
   Set<bool>  sf = new Set.from([false, false, true]);
   Set<void> sg = new Set.from([null, null, null]);
+  Set<List> sList = new Set.from([[1,2,3], new List()]);
+  Set<Set> sSet = new Set.from([new Set(), new Set.from([])]);
+  Set<Map> sMap = new Set.from([new Map(), {}]);
+  Set<Function> sFunction = new Set.from([(){}, (){}]);
+  Set<MyClass> sMyClass = new Set.from([new MyClass(), new MyClass()]);
   
+  Set<List<String>> slString = new Set.from([lString]);
+  Set<List<int>> slInt = new Set.from([lInt]);
+  Set<List<double>> slDouble = new Set.from([lDouble]);
+  Set<List<bool>> slBool = new Set.from([lBool]);
+  Set<List<void>> slVoid = new Set.from([lVoid]);
+  Set<List<List>> slList = new Set.from([lList]);
+  Set<List<Set>> slSet = new Set.from([lSet]);
+  Set<List<Map>> slMap = new Set.from([lMap]);
+  Set<List<Function>> slFunction = new Set.from([lFunction]);
+  Set<List<MyClass>> slMyClass = new Set.from([lMyClass]);
+   
   Map ma = new Map();
-  
+  Map<String, dynamic> mdynamic = {"key1":1, "key2":"2"};
+  Map<String, String> mString = {"key1":"문자열", "key2":"값"};
+  Map<String, int> mInt = {"key1":1, "key2":2};
+  Map<String, double> mDouble = {"key1": 1.0, "key2": 2.3};
+  Map<String, bool> mBool = {"key1": true, "key2": false};
   
   Function fa = (){};
-  Function fb = () => a;
+  Function fb = () => 'a';
   
   
   MyClass mya = new MyClass();
