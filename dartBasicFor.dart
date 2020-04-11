df1b04070d42634ef1d917d18437c933aac51ab3
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-04-11 12:30:05 +0900

    ing
    ing

--- dartBasicFor.dart
+++ dartBasicFor.dart
@@ -1,96 +1,44 @@
-void main(){
-  // For & Find
-  
-// String
+void main() {
 
-//   String value = "abcdefg";
-//   String target = "";
+  // 문자열 
   
-//   // 1 For문
-//   for(int _i = 0; _i < value.length; _i++){
-//     print(value);
-//     print(_i);
-//   }
+  // 분석
   
-//   // String Indexing
-//   for(int _i = 0; _i < value.length; _i++){
-//     print(value[_i]);
-//   }
-  
-//   // bool
-//     for(int _i = 0; _i < value.length; _i++){
-//       print(value[_i] == target);
-//     }
-  
-//   //if
-//   target = "c";
-//   for(int _i = 0; _i < value.length; _i++){
-//     print(value[_i]);
-//     if(value[_i] == target){
-//       print("Find It!");
-//     }
-//   }
-  
-//   //if & return
-//   target = "d";
-  
-//   for(int _i = 0; _i < value.length; _i++){
-//     print(value[_i]);
-//     if(value[_i] == target){
-//       return print("Find It!");
-//     }
-//   }
-  
-  
- // List
-  
-//   List<int> list = [1,2,3,4,5,6,7];
-  
-//   List<int> forList = list.map((e) => e).toList();
-//   print(forList);
-  
-//   List<int> newLists;
-  
-//   // List<int> newNewLists = list.forEach((e) => print(e));
-//   // list.forEach((e) => newLists.add(e));
-//   // print(newLists);
-//   list.forEach((e) => print(e));
-  
-//   void email({int i}) => print(i.toString());
-//   list.forEach((e) => email(i:e)); 
-//   //.forEach는 변수에 담을 수도, 내부에서 외부 컨텍스트를 사용하기 불편하지만
-//   // 내부 컨텍스트에서는 자유롭기 때문에 void 기능을 반복 작업할 때 사용하기 좋음
-  
-//   //요소 찾기
-//   var v;
-//   v = list.indexOf(1);
-//   print(v);
-//   v = list.indexOf(8);
-//   print(v); // 없으면 -1 // 있으면 있는 위치의 index 값
-  
-//   v = list.map((e) => list.indexOf(e)).toList();
-//   print(v);
-  
+  String value = " (빈) 문자열 분석 (공간) ";
  
-//   int target = 6;
+  print(value[0]);
+  print(value[1]);
+  print(value[2]);
+  print(value[3]);
+  try{
+    print(value[11] == null);
+  }
+  catch(e){
+    print("value 변수에 없는 index");
+  }
   
-//   v = list.map((e) => e == target).toList();
-//   print(v);
+//   print(value[11] == null);
   
-//   v = list.map((int e){
-//     if(e == target){
-//       return e;
-//     }
-//     else{
-//       return null;
-//     }
-//   }).toList();
-//   print(v);
+//   print(value[11]);
   
-
+  print(value.length);
   
   
+  for(int i = 0; i < value.length; i++){
+    print("$i 순번에 값은 : ${value[i]}"); // 빈공간, 특수기호도 값으로 찾을 수 있다
+  }
   
+  
+  String customValue = value.trim();
+  print(customValue); // 문자열의 가장 앞뒤 공백을 지운다
+  
+  customValue = value.split(' ')[0]; print(customValue);  
+  customValue = value.split(' ')[1]; print(customValue);  
+  customValue = value.split(' ')[2]; print(customValue);  
+  customValue = value.split(' ')[3]; print(customValue);  
+  
+  List customValueSplit = value.split(' ');
+  print(customValueSplit); // 공란을 기준으로 값을 나누어 배열로 만들어줍니다
   
   
 }
