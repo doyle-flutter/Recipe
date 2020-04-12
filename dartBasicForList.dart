7941fef9efe7adc3209d6f7068b451610d7069a3
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-04-12 13:24:24 +0900

    ing
    ing

--- dartBasicForList.dart
+++ dartBasicForList.dart
@@ -15,13 +15,47 @@ main(){
   print(checkIndex);
   
   checkIndex = target.lastIndexOf('가');
-  print(checkIndex);
+  print(checkIndex); // 뒤에서부터 찾아도 index 순번이 변하지는 않는다, List의 첫번째 요소는 언제나 0
   
   checkIndex = target.indexOf("a");
   print(checkIndex);
   
   checkIndex = target.lastIndexOf("a");
   print(checkIndex);
+  
+  
+  // for & add
+  
+  for(int i = 0; i < target.length; i++){
+    print(target[i]);
+  }
+  
+  List newTarget1 = [];
+  for(int i = 0; i < target.length; i++){
+    newTarget1.add("NEW 1 ${target[i]}");
+  }
+  print(newTarget1);
+  
+  List newTarget2 = [];
+  target.forEach((e){
+    newTarget2.add("NEW 2 $e");
+  });
+  print(newTarget2);
+  
+  
+  List newTarget3 = target.map((e) => "NEW 3 "+e).toList();
+  print(newTarget3);
+  
+  // sort 
+  List<int> numList = [1,2,3,4,5];
+  target.sort((String start, String end) => end.hashCode.compareTo(start.hashCode));
+  print(target);
+  // for & find
+ 
+  
+  
+  
+  
   
   
   
