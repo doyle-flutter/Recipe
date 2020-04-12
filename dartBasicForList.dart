f080c0ef5930a7d93797b085a9197cf0cb467f79
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-04-13 04:51:31 +0900

    dartBasicForList
    dartBasicForList

--- dartBasicForList.dart
+++ dartBasicForList.dart
@@ -4,32 +4,13 @@ main(){
   
   List<String> target = ['가', '나', '다'];
   
+  print(value);
+  print(target);
+  
   // index
-  print(target[0]);
+  print( target[0] );
   // length
   print( target.length );
-  
-  
-  // find
-  int checkIndex = target.indexOf("가");
-  print(checkIndex);
-  
-  checkIndex = target.lastIndexOf('가');
-  print(checkIndex); // 뒤에서부터 찾아도 index 순번이 변하지는 않는다, List의 첫번째 요소는 언제나 0
-  
-  checkIndex = target.indexOf("a");
-  print(checkIndex);
-  
-  checkIndex = target.lastIndexOf("a");
-  print(checkIndex);
-  
-  
-  String findValue = '가';
-  for(int i =0; i<target.length; i++){
-    // if(target[i] == findValue) return print("찾음"); // main Context가 return 되어 종료
-    if(target[i] == findValue) print("찾음");
-  }
-  
   
   // for & add
   
@@ -48,13 +29,52 @@ main(){
     newTarget2.add("NEW 2 $e");
   });
   print(newTarget2);
-  
-  
+    
   List newTarget3 = target.map((e) => "NEW 3 "+e).toList();
   print(newTarget3);
   
+
+  
+  
+  // find
+  // 뒤에서부터 찾아도 index 순번이 변하지는 않는다, 
+  // List의 첫번째 요소는 언제나 0
+  
+  
+  int checkIndex = target.indexOf("가");
+  print(checkIndex);
+  
+  checkIndex = target.lastIndexOf('가');
+  print(checkIndex); 
+
+  checkIndex = target.indexOf("a");
+  print(checkIndex);
+  
+  checkIndex = target.lastIndexOf("a");
+  print(checkIndex);
+  
+  
+  // main Context가 return 되어 종료
+  
+  
+  String findValue1 = '가';
+  for(int i =0; i<target.length; i++){
+    if(target[i] == findValue1) print("찾음");
+    print(target[i]);
+  }
+  
+  String findValue2 = '가';
+  for(int i =0; i<target.length; i++){
+    // if(target[i] == findValue2) return print("찾음"); 
+    print(target[i]);
+  }
+  
+
+  
   // sort 
-  List<int> numList = [1,2,3,4,5];
+  
+  List<int> numList = [1,2,5,4,3];
+  
   numList.sort((int start, int end) => end.compareTo(start));
   print(numList);
   numList.sort((int start, int end) => start.compareTo(end));
