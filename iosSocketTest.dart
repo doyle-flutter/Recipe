a50381b0dcbec82af79fdf22ef1fdb0b9a1aefd6
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-05-13 20:39:11 +0900

    ios socket test ex

---
+++ iosSocketTest.dart
@@ -0,0 +1,62 @@
+// 급하게 IOS 테스트한 예제 코드만 업로드합니다 :)
+// 패키지 : socket_io_client: ^0.9.10
+
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  IO.Socket socket;
+
+  @override
+  void initState() {
+    socket = IO.io('http://localhost:3000/',<String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+    socket.connect();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: IconButton(
+          icon: Icon(Icons.data_usage),
+          onPressed: (){
+            socket.emit("send_message", "VALUE");
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+/// Node.JS
+
+// var app = require('express')(); 
+// var http = require('http').Server(app); 
+// var io = require('socket.io')(http); 
+
+// app.get('/', function(req, res){ 
+//     res.send("hi"); 
+// }); 
+// // app.get("/chat",(req,res) =>{
+// //     res.sendFile('../index.html');
+// // });
+
+
+// io.on('connection', function(socket){ 
+//     socket.on('send_message', function(msg){ 
+//         console.log(msg)
+//         io.emit('receive_message', msg); }
+//     ); 
+// });
+
+
+// http.listen(3000, function(){ console.log('listening on *:3000'); });
