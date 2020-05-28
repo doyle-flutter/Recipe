f7024cf46bcf0b3df68a777489f5e0a81463c8f0
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-05-29 01:19:26 +0900

    Update naverLogin.dart

--- naverLogin.dart
+++ naverLogin.dart
@@ -1,33 +1,33 @@
 Container(
-              width: MediaQuery.of(context).size.width/2,
-              child: RaisedButton(
-                color: Colors.greenAccent[700],
-                textColor: Colors.white,
-                child: Text("Naver Login"),
-                onPressed: () async{
-                  String naver_url = "http://127.0.0.1:3000/naverlogin";
-                  String _nToken = await Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => WebviewScaffold(
-                        url: naver_url,
-                        javascriptChannels: Set.from([
-                          JavascriptChannel(
-                            name: "james",
-                            onMessageReceived: (JavascriptMessage result) async{
-                              if(result.message != null) return Navigator.of(context).pop(result.message);
-                              return Navigator.of(context).pop();
-                            }
-                          ),
-                        ]),
-                      )
-                    )
-                  );
-                  if(_nToken != null) return Navigator.of(context).pushAndRemoveUntil(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => MainPage(userName: _textEditingController.text)
-                    ),
-                    (route) => false
-                  );
-                },
+  width: MediaQuery.of(context).size.width/2,
+  child: RaisedButton(
+    color: Colors.greenAccent[700],
+    textColor: Colors.white,
+    child: Text("Naver Login"),
+    onPressed: () async{
+      String naver_url = "http://127.0.0.1:3000/naverlogin";
+      String _nToken = await Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (BuildContext context) => WebviewScaffold(
+            url: naver_url,
+            javascriptChannels: Set.from([
+              JavascriptChannel(
+                name: "james",
+                onMessageReceived: (JavascriptMessage result) async{
+                  if(result.message != null) return Navigator.of(context).pop(result.message);
+                  return Navigator.of(context).pop();
+                }
               ),
-            )
+            ]),
+          )
+        )
+      );
+      if(_nToken != null) return Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (BuildContext context) => MainPage(userName: _textEditingController.text)
+        ),
+        (route) => false
+      );
+    },
+  ),
+)
