6f5ce728154acf0f4b76a2cc4fba0508e6c5dce1
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-07-31 12:49:23 +0900

    작성중...

--- autologin/app.js
+++ autologin/app.js
@@ -6,15 +6,15 @@ app.use(express.urlencoded({extended:false}));
 
 app.get('/',(req,res) => res.send("Hi !"));
 app.post('/login', (req,res) => {
-    var headerToken = req.headers.token;
+    var headerToken = req.headers['token'];
     var id = req.body['id'];
-    var token = req.body['token'];
-    if(id === undefined || token === undefined) return res.json(false);
-    if(id !== "qwe" && token !== "123") return res.json(false); // id & token Check + Validation Logic
+    var pw = req.body['pw'];
+    if(id === undefined || pw === undefined) return res.json(false);
+    if(id !== "qwe" && pw !== "123") return res.json(false); // id & token Check + Validation Logic
     if(headerToken === undefined) return res.json(false);
     if(headerToken !== "1") return res.json(false); // DB Token Check Logic
-    return res.json({'id':"id", "token":"1", "pw":"123"});
-    
+    console.log(true);
+    return res.json(true);
 });
 
 app.listen(3000);
