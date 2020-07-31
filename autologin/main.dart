7918934dd37d6dedb0eda196c2754297a344a35d
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2020-07-31 12:49:00 +0900

    중간..

--- autologin/main.dart
+++ autologin/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -7,98 +8,303 @@ import 'package:http/http.dart' as http;
 void main() => runApp(
   MultiProvider(
     providers: [
-      ChangeNotifierProvider<LoginProvider>(create: (_) => new LoginProvider(),)
+      ChangeNotifierProvider<LoginCheckProvider>(create: (_) => new LoginCheckProvider(),)
     ],
-    child: MaterialApp(
-      home: LoginCheckViewModel(),
-    ),
+    child: MaterialApp(home: AutoLoginCheck(),)
   )
 );
 
-class LocalDbfLoginCheck{
-  static const String _CHECK_KEY = "loginCheck";
-  static const String _ID_KEY = "idCheck";
-  static const String _TOKEN_KEY = "tokenCheck";
-  Future<LocalDbfUserInfo> localDbfCheck() async => await SharedPreferences.getInstance().then((SharedPreferences _pref){
-    final bool _check =_pref.getBool(_CHECK_KEY) ?? false;
-    final String _id = _pref.getString(_ID_KEY) ?? "";
-    final String _token = _pref.getString(_TOKEN_KEY) ?? "";
-    return LocalDbfUserInfo.getData(id: _id, token: _token, check: _check);
+class LocalDbf{
+  static const String _AUTO_KEY = "auto";
+  static const String _AUTO_ID = "aid";
+  static const String _AUTO_TOKEN = "atoken";
+  Future<bool> autoCheck() async => await SharedPreferences.getInstance().then((SharedPreferences _pref) => _pref.getBool(_AUTO_KEY) ?? false);
+  Future<bool> setCheck({@required bool auto}) async => await SharedPreferences.getInstance().then((SharedPreferences _pref) async => await _pref.setBool(_AUTO_KEY, auto));
+  Future<Map<String, String>> userInfo() async => await SharedPreferences.getInstance()
+    .then((SharedPreferences _pref){
+      final String _id = _pref.getString(_AUTO_ID) ?? "";
+      final String _pwnToken = _pref.getString(_AUTO_TOKEN) ?? "";
+      if(_id.isEmpty || _pwnToken.isEmpty) return {};
+      return {"id":_id, "pw": _pwnToken};
+    });
+  Future<bool> setUserInfo({@required String id, @required String pwnToken}) async => await SharedPreferences.getInstance().then((SharedPreferences _pref) async{
+    final bool _setId = await _pref.setString(_AUTO_ID, id);
+    final bool _setPw = await _pref.setString(_AUTO_TOKEN, pwnToken);
+    if(!_setId || !_setPw) return false;
+    return true;
   });
-  Future<bool> localDbfSet({@required bool check}) async => await SharedPreferences.getInstance().then((SharedPreferences _pref) => _pref.setBool(_CHECK_KEY, check)).then((bool _saveCheck) => _saveCheck);
+  Future<bool> clearAutoInfo() async => await SharedPreferences.getInstance().then((SharedPreferences _pref) async => await _pref.clear() ?? false);
 }
 
-class LocalDbfUserInfo{
-  final String id;
-  final String token;
-  final bool check;
-  const LocalDbfUserInfo({@required this.id, @required this.token, @required this.check});
-  factory LocalDbfUserInfo.getData({@required String id, @required String token, @required bool check}) => new LocalDbfUserInfo(id: id, token: token,check: check);
-}
-
-class UserLoginInfo{
-  static Map<String, String> toJson({@required String id, @required String token}) => {'id':id, 'token':token};
-}
-
-class UserInfo{
-  final String id;
-  final String pw;
-  const UserInfo({@required this.id, @required this.pw}):assert(id != null), assert(pw != null);
-  factory UserInfo.setData({@required Map<String, dynamic> json}) => new UserInfo(id: json['id'] as String, pw: json['pw'] as String);
-}
-
-class ConnectServer{
-  static const String _END_POINT = "http://192.168.0.2:3000";
-  static const Map<String, String> _HEADER_SETTING = {"token":"1"}; // JWT, sha ... Logic
-  Future<Map<String, dynamic>> _loginPost({@required String id, @required String token}) async{
-    const String _url = "$_END_POINT/login";
+class Fetch{
+  static const String _END_POINT = "http://127.0.0.1:3000";
+  Future<bool> postFetch({@required Map<String, dynamic> body, @required Map<String, String> header}) async{
     try{
-      final http.Response _res = await http.post(_url, headers: _HEADER_SETTING, body: UserLoginInfo.toJson(id: id, token: token)).timeout(Duration(seconds: 8), onTimeout: () async => new http.Response("false", 400));
-      final Map<String, dynamic> _userInfoJson = json.decode(_res.body);
-      return _userInfoJson;
+      http.Response _res = await http.post("$_END_POINT/login",headers: header, body: body).timeout(Duration(seconds: 8),onTimeout: () async => http.Response("false", 400));
+      bool _result = json.decode(_res.body);
+      return _result;
     }
     catch(e){
-      return null;
-    }
-  }
-}
-
-class LoginPost extends ConnectServer{
-  Future<UserInfo> login({@required String id, @required String token}) async{
-    final Map<String, dynamic> _result = await super._loginPost(id: id, token: token) ?? {};
-    if(_result.isEmpty) return null;
-    return UserInfo.setData(json: _result); //{id}
-  }
-}
-
-class LoginProvider with ChangeNotifier{
-  LocalDbfLoginCheck _loginCheck = new LocalDbfLoginCheck();
-  LoginPost _post = new LoginPost();
-  Future<bool> autoLoginCheck() async{
-    LocalDbfUserInfo _user = await _loginCheck.localDbfCheck();
-    if(!_user.check || _user.id.isEmpty || _user.token.isEmpty){
       return false;
     }
-    UserInfo _connectUserInfo = await _post.login(id: _user.id, token: _user.token);
-    if(_connectUserInfo == null){
-      return false;
-    }
-    return true;
   }
-  // net -> setter
+  void _parse(){} // ... 반환 구조 지정 필요
 }
 
-class LoginCheckViewModel extends StatelessWidget {
+class LoginCheckProvider with ChangeNotifier{
+  final LocalDbf _dbf = new LocalDbf();
+  final Fetch _fetch = new Fetch();
+
+  bool result;
+
+  LoginCheckProvider(){
+    Future.microtask(() async => await check());
+    return;
+  }
+
+  Future<void> check() async{
+    final bool _check = await _dbf.autoCheck();
+    if(!_check) return this.result = false;
+    final Map<String, String> _body = await _dbf.userInfo();
+    if(_body.isEmpty) return this.result = false;
+    this.result = await _fetch.postFetch(body: _body, header: {"token":"1"});
+    notifyListeners();
+  }
+
+  Future<void> logOut() async{
+    this.result = !await _dbf.clearAutoInfo();
+    notifyListeners();
+    return;
+  }
+
+  Future<void> login({@required String id, @required String pwnToken}) async{
+    final bool _res = await _fetch.postFetch(body: {'id':id, "pw":pwnToken}, header: {"token":"1"});
+    if(_res){
+      await _dbf.setCheck(auto: true);
+      await _dbf.setUserInfo(id: id, pwnToken: pwnToken);
+      this.result = true;
+    }
+    notifyListeners();
+  }
+}
+
+class AutoLoginCheck extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    LoginProvider _lg = Provider.of<LoginProvider>(context);
-    return FutureBuilder(
-      future: _lg.autoLoginCheck(),
-      builder: (BuildContext context,AsyncSnapshot<bool> snap){
-        return Scaffold(
-          appBar: AppBar(title: Text(snap.data.toString()),),
-        );
+    LoginCheckProvider _lc = Provider.of<LoginCheckProvider>(context);
+    return _lc.result == null
+      ? Scaffold(body: Text("로딩"))
+      : _lc.result
+        ? new MainPage()
+        : new LoginPage();
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  FocusNode _idFocus;
+  FocusNode _pwFocus;
+
+  TextEditingController _idController;
+  TextEditingController _pwController;
+  
+  @override
+  void initState() {
+    if(!mounted) return;
+    Future.microtask((){
+      _idFocus = new FocusNode();
+      _pwFocus = new FocusNode();
+      _idController = new TextEditingController();
+      _pwController = new TextEditingController();
+      return;
+    }).then((_) => setState((){}));
+    super.initState();
+  }
+  
+  @override
+  void dispose() {
+    _idController?.dispose();
+    _pwController?.dispose();
+    super.dispose();
+  }
+
+  LoginCheckProvider _lc;
+
+  @override
+  Widget build(BuildContext context) {
+    if(_lc == null) _lc = Provider.of<LoginCheckProvider>(context);
+    return IgnorePointer(
+      ignoring: _idController == null || _pwController == null ? true : false,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Flexible(
+                  flex: 2,
+                  child: Container(
+                    margin: EdgeInsets.all(50.0),
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://raw.githubusercontent.com/doyle-flutter/Recipe/master/2019-11-21.webp",
+                          ),
+                          fit: BoxFit.cover,
+                        )
+                    ),
+                  )
+              ),
+              Flexible(
+                  child: Container(
+                    child: Form(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Flexible(
+                            child: Container(
+                              width: MediaQuery.of(context).size.width/2,
+                              alignment: Alignment.center,
+                              child: TextField(
+                                maxLines: 1,
+                                focusNode: _idFocus,
+                                textInputAction: TextInputAction.next,
+                                cursorColor: Colors.black,
+                                controller: _idController,
+                                autocorrect: true,
+                                enabled: true,
+                                keyboardType: TextInputType.emailAddress,
+                                onSubmitted: (String v)
+                                => this._inputFieldFocusChange(
+                                    context: context,
+                                    currentFocus: this._idFocus,
+                                    nextFocus: this._pwFocus
+                                ),
+                                decoration: InputDecoration(
+                                    filled: true,
+                                    fillColor: Colors.grey[200],
+                                    hintText: "E-Mail",
+                                    prefixIcon: Icon(Icons.person,color: Colors.black,),
+                                    suffixIcon: this._inputIcon(controller: this._idController, currentFocus: this._idFocus),
+                                    border: InputBorder.none
+                                ),
+                              ),
+                            ),
+                          ),
+                          Flexible(
+                            child: Container(
+                              width: MediaQuery.of(context).size.width/2,
+                              alignment: Alignment.center,
+                              child: TextField(
+                                maxLines: 1,
+                                focusNode: _pwFocus,
+                                textInputAction: TextInputAction.done,
+                                cursorColor: Colors.black,
+                                controller: _pwController,
+                                obscureText: true,
+                                onSubmitted: (String v){},
+                                decoration: InputDecoration(
+                                    filled: true,
+                                    fillColor: Colors.grey[200],
+                                    hintText: "Password",
+                                    prefixIcon: Icon(Icons.lock_outline, color: Colors.black,),
+                                    suffixIcon: this._inputIcon(
+                                        controller: this._pwController,
+                                        currentFocus: this._pwFocus),
+                                    border: InputBorder.none
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+              ),
+              Flexible(
+                  child: Column(
+                    children: <Widget>[
+                      Flexible(
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: MaterialButton(
+                            onPressed: this._validation,
+                            child: Text("Login"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _inputIcon({TextEditingController controller, FocusNode currentFocus}){
+    if(controller?.text == "") return null;
+    else return IconButton(
+      icon: Icon(Icons.clear),
+      onPressed: (){
+        this.clickClearText(targetVar: controller, currentFocus: currentFocus);
+        print(controller?.text);
       },
+    );
+  }
+
+  void _inputFieldFocusChange({
+    BuildContext context,
+    FocusNode currentFocus,
+    FocusNode nextFocus
+  }){
+    Future.microtask(() => currentFocus.unfocus())
+        .then((_) => FocusScope.of(context).requestFocus(nextFocus));
+  }
+
+  void clickClearText({
+    @required TextEditingController targetVar,
+    @required FocusNode currentFocus
+  }){
+    assert(targetVar != null); assert(currentFocus != null);
+    Future.delayed(Duration.zero,(){
+      currentFocus.unfocus();
+    }).then((_) => targetVar.clear());
+  }
+
+  Future<void> _validation() async{
+    print(_idController.text);
+    print(_pwController.text);
+    bool _isCheck = false;
+    await _lc.login(id: _idController.text, pwnToken: _pwController.text);
+    _idController.text = "";
+    _pwController.text = "";
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    LoginCheckProvider _lc = Provider.of<LoginCheckProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.error_outline),
+            onPressed: () async {
+              await _lc.logOut();
+            },
+          )
+        ],
+      ),
     );
   }
 }
