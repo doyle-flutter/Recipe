646953b90c88e8eec2f2bd6ccfd820450e9082cb
James-dev <56661529+doyle-flutter@users.noreply.github.com>
2020-10-28 11:28:52 +0900

    Update and rename dartFunction.dart to dartArg.dart

--- dart/dartArg.dart
+++ dart/dartArg.dart
@@ -1,4 +1,4 @@
-// 문법이 중요한 이유
+// Cafe : 문법이 중요한 이유
 // https://cafe.naver.com/flutterjames/38
 
 void main() {
@@ -19,10 +19,24 @@ void main() {
 }
 
 class MyClass{
-  MyClass( 
-    int c, { int a, String b } );
+  MyClass( int c, { int a, String b } );
   void mFF( int c, { int a, String b } ){}
-  
   static mFF2( int c, { int a, String b } ){}
+}
+
+class MyClass2{
+  int a;
+  String b;
+  int c;
+  MyClass2( this.c, {this.a, this.b}) : assert(a != null), assert(b != null), assert(c != null);
+  void mFF( int c, { int a, String b } ){
+    print(a);
+    print(this.a);
+    return;
+  }
+  static mFF2( int c, { int a, String b } ){
+    print(a);
+    print(MyClass2(1,a:123, b:"321").a);
+  }
   
 }
